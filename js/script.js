19745c9e2e27ae89bd9d6cb237bf000de40b6328
Patrick Kontschak <github@patrickkontschak.com>
2020-06-06 11:34:53 +0200

    style tabs and add topic titles to dom

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const apiUrl = 'http://localhost:8000/api.php'
+const tabbed = document.querySelector('.tabbed');
 
 const getSites = async () => {
     const response = await fetch(apiUrl)
     const json = await response.json()
     const topics = json.topics
     const posts = json.posts
+    let topicsMarkup = ""
+    
+    topics.map(topic => topicsMarkup += `<p>${topic.title}</p>`)
 
     // console.log(topics)
     // console.log(posts)
+    
+    tabbed.querySelector('#section-topics').innerHTML = topicsMarkup
 
     // console.log(json)
 }
