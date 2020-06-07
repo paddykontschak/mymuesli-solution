64a8b9fd41cac7a0fbe42b35f4f708373d35a94e
Patrick Kontschak <github@patrickkontschak.com>
2020-06-07 13:43:56 +0200

    add switchTab function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-const apiUrl = 'http://localhost:8000/api.php'
-const tabbed = document.querySelector('.tabbed');
+const apiUrl  = 'http://localhost:8000/api.php'
+const tabbed  = document.querySelector('.tabbed')
+const tablist = tabbed.querySelector('ul')
+const tabs    = tablist.querySelectorAll('a')
+const panels  = tabbed.querySelectorAll('[id^="section"]')
 
 const getSites = async () => {
-    const response = await fetch(apiUrl)
-    const json = await response.json()
-    const topics = json.topics
-    const posts = json.posts
-    let topicsMarkup = ""
+    const response     = await fetch(apiUrl)
+    const json         = await response.json()
+    const topics       = json.topics
+    const posts        = json.posts
+    let topicsMarkup   = ""
     let archivesMarkup = ""
-    let pagesMarkup = ""
+    let pagesMarkup    = ""
 
     posts.map(post => {
         post.topics.map(topicId => {
@@ -51,68 +54,77 @@ const getSites = async () => {
 
 getSites()
 
-// The tab switching function
-// const switchTab = (oldTab, newTab) => {
-//     newTab.focus();
-//     // Make the active tab focusable by the user (Tab key)
-//     newTab.removeAttribute('tabindex');
-//     // Set the selected state
-//     newTab.setAttribute('aria-selected', 'true');
-//     oldTab.removeAttribute('aria-selected');
-//     oldTab.setAttribute('tabindex', '-1');
-//     // Get the indices of the new and old tabs to find the correct
-//     // tab panels to show and hide
-//     let index = Array.prototype.indexOf.call(tabs, newTab);
-//     let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
-//     panels[oldIndex].hidden = true;
-//     panels[index].hidden = false;
-// }
+// source: https://codepen.io/heydon/pen/veeaEa
+const switchTab = (oldTab, newTab) => {
+    newTab.focus()
+    newTab.removeAttribute('tabindex')
+    newTab.setAttribute('aria-selected', 'true')
 
-// // Add the tablist role to the first <ul> in the .tabbed container
-// tablist.setAttribute('role', 'tablist');
+    oldTab.removeAttribute('aria-selected')
+    oldTab.setAttribute('tabindex', '-1')
 
-// // Add semantics are remove user focusability for each tab
-// Array.prototype.forEach.call(tabs, (tab, i) => {
-//     tab.setAttribute('role', 'tab');
-//     tab.setAttribute('id', 'tab' + (i + 1));
-//     tab.setAttribute('tabindex', '-1');
-//     tab.parentNode.setAttribute('role', 'presentation');
-    
-//     // Handle clicking of tabs for mouse users
-//     tab.addEventListener('click', e => {
-//     e.preventDefault();
-//     let currentTab = tablist.querySelector('[aria-selected]');
-//     if (e.currentTarget !== currentTab) {
-//         switchTab(currentTab, e.currentTarget);
-//     }
-//     });
-    
-//     // Handle keydown events for keyboard users
-//     tab.addEventListener('keydown', e => {
-//     // Get the index of the current tab in the tabs node list
-//     let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-//     // Work out which key the user is pressing and
-//     // Calculate the new tab's index where appropriate
-//     let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-//     if (dir !== null) {
-//         e.preventDefault();
-//         // If the down key is pressed, move focus to the open panel,
-//         // otherwise switch to the adjacent tab
-//         dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-//     }
-//     });
-// });
+    let index    = Array.prototype.indexOf.call(tabs, newTab)
+    let oldIndex = Array.prototype.indexOf.call(tabs, oldTab)
 
-// // Add tab panel semantics and hide them all
-// Array.prototype.forEach.call(panels, (panel, i) => {
-//     panel.setAttribute('role', 'tabpanel');
-//     panel.setAttribute('tabindex', '-1');
-//     let id = panel.getAttribute('id');
-//     panel.setAttribute('aria-labelledby', tabs[i].id);
-//     panel.hidden = true; 
-// });
+    panels[oldIndex].hidden = true
+    panels[index].hidden    = false
+}
 
-// // Initially activate the first tab and reveal the first tab panel
-// tabs[0].removeAttribute('tabindex');
-// tabs[0].setAttribute('aria-selected', 'true');
-// panels[0].hidden = false;
+Array.prototype.forEach.call(tabs, (tab, i) => {
+    tab.setAttribute('role', 'tab')
+    tab.setAttribute('id', 'tab' + (i + 1))
+    tab.setAttribute('tabindex', '-1')
+
+    tab.addEventListener('click', event => {
+        event.preventDefault()
+        let currentTab = tablist.querySelector('[aria-selected]')
+
+        if (event.currentTarget !== currentTab) {
+            switchTab(currentTab, event.currentTarget)
+        }
+    });
+
+    tab.addEventListener('keydown', event => {
+        let index = Array.prototype.indexOf.call(tabs, event.currentTarget)
+        let direction
+
+        switch (event.which) {
+            case 37:
+                direction = index - 1
+                break
+            case 39:
+                direction = index + 1
+                break
+            case 40:
+                direction = 'down'
+                break
+            default:
+                direction = null
+                break
+        }
+
+        if (direction !== null) {
+            event.preventDefault();
+
+            if (direction === 'down') {
+                panels[i].focus()
+            } else if (tabs[direction]) {
+                switchTab(event.currentTarget, tabs[direction])
+            } else {
+                void 0
+            }
+        }
+    });
+});
+
+Array.prototype.forEach.call(panels, (panel, i) => {
+    panel.setAttribute('role', 'tabpanel')
+    panel.setAttribute('tabindex', '-1')
+    panel.setAttribute('aria-labelledby', tabs[i].id)
+    panel.hidden = true
+});
+
+const selectedTabIndex = Math.floor(Math.random() * tabs.length)
+tabs[selectedTabIndex].removeAttribute('tabindex')
+tabs[selectedTabIndex].setAttribute('aria-selected', 'true')
+panels[selectedTabIndex].hidden = false
